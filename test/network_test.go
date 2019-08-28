3a781dc5cce95cd26119fa6b481325700da2d2e3
Yox2Yox <ingentity@gmail.com>
2019-08-28 22:47:48 +0900

    networktest複数ワーカに対応

--- test/network_test.go
+++ test/network_test.go
@@ -2,6 +2,7 @@ package test
 
 import (
 	"context"
+	"strconv"
 	"testing"
 	"time"
 	"yox2yox/antone/bridge"
@@ -16,7 +17,18 @@ import (
 func Test_CreateNetWork(t *testing.T) {
 
 	clientId := "client0"
-	workerId := "worker0"
+	workersId := []string{
+		"worker0",
+		"worker1",
+		"worker2",
+		"worker3",
+		"worker4",
+		"worker5",
+	}
+	workersAddr := "localhost"
+	workersBasePort := 10000
+
+	var wpeers []*wPeer.Peer
 
 	//ブリッジPeer起動
 	bridgeConfig, err := bConfig.ReadBridgeConfig()
@@ -41,18 +53,22 @@ func Test_CreateNetWork(t *testing.T) {
 	if err != nil {
 		t.Fatalf("want no error, but has error %#v", err)
 	}
-	wpeer, err := wPeer.New(workerConfig.Server, false)
-	if err != nil {
-		t.Fatalf("want no error, but has error %#v", err)
-	}
-	ctxworker, cancelworker := context.WithTimeout(context.Background(), 10*time.Second)
-	go func() {
-		err = wpeer.Run(ctxworker)
+	for i, _ := range workersId {
+		workerConfig.Server.Addr = workersAddr + ":" + strconv.Itoa(workersBasePort+i)
+		wpeer, err := wPeer.New(workerConfig.Server, false)
 		if err != nil {
 			t.Fatalf("want no error, but has error %#v", err)
 		}
-	}()
-	defer cancelworker()
+		wpeers = append(wpeers, wpeer)
+		ctxworker, cancelworker := context.WithTimeout(context.Background(), 10*time.Second)
+		go func() {
+			err = wpeer.Run(ctxworker)
+			if err != nil {
+				t.Fatalf("want no error, but has error %#v", err)
+			}
+		}()
+		defer cancelworker()
+	}
 
 	//ブリッジクライアント作成
 	connBridge, err := grpc.Dial(bpeer.Config.Server.Addr, grpc.WithInsecure())
@@ -64,33 +80,37 @@ func Test_CreateNetWork(t *testing.T) {
 	ctxOrder, orderCancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer orderCancel()
 
-	//ワーカクライアント作成
-	connWorker, err := grpc.Dial(wpeer.ServerConfig.Addr, grpc.WithInsecure())
-	if err != nil {
-		t.Fatalf("want no error,but error %#v", err)
-	}
-	defer connWorker.Close()
+	//ワーカクライアント作成(未使用)
+	// connWorker, err := grpc.Dial(workerConfig.Server.Addr, grpc.WithInsecure())
+	// if err != nil {
+	// 	t.Fatalf("want no error,but error %#v", err)
+	// }
+	// defer connWorker.Close()
+
 	//clientWorker := wpb.NewWorkerClient(connWorker)
 	//ctxWorker, workerCancel := context.WithTimeout(context.Background(), 10*time.Second)
 	//defer workerCancel()
 
 	//各種アカウント登録
-	_, err = bpeer.Accounting.CreateNewWorker(workerId, wpeer.ServerConfig.Addr)
-	if err != nil {
-		t.Fatalf("want no error,but error %#v", err)
+	for i, workerId := range workersId {
+		_, err = bpeer.Accounting.CreateNewWorker(workerId, wpeers[i].ServerConfig.Addr)
+		if err != nil {
+			t.Fatalf("want no error,but error %#v", err)
+		}
 	}
 
 	_, err = bpeer.Accounting.CreateNewClient(clientId)
 	if err != nil {
 		t.Fatalf("want no error,but error %#v", err)
 	}
-
-	data, err := wpeer.DataPool.GetDataPool(clientId)
-	if err != nil {
-		t.Fatalf("want no error,but error %#v", err)
-	}
-	if data != 0 {
-		t.Fatalf("want user's data==0,but %#v", data)
+	for _, wpeer := range wpeers {
+		data, err := wpeer.DataPool.GetDataPool(clientId)
+		if err != nil {
+			t.Fatalf("want no error,but error %#v", err)
+		}
+		if data != 0 {
+			t.Fatalf("want user's data==0,but %#v", data)
+		}
 	}
 
 	t.Log("Start to get validatable code")
@@ -107,5 +127,7 @@ func Test_CreateNetWork(t *testing.T) {
 	if vCode.Data != 0 {
 		t.Fatalf("want vcode.data == 0,but %#v", vCode.Data)
 	}
+
+	time.Sleep(3 * time.Second)
 
 }
