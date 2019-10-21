0b36a29a11143099d8696609a9f88d890c65fa63
waradin <kuajiyoshi@gmail.com>
2019-10-21 20:53:37 +0900

    credibility追加

---
+++ pkg/stolerance/creability_test.go
@@ -0,0 +1,114 @@
+package stolerance
+
+import (
+	"os"
+	"testing"
+	"yox2yox/antone/internal/log2"
+)
+
+var (
+	groups1 = [][]float64{
+		[]float64{
+			0.3,
+			0.6,
+			0.2,
+			0.1,
+		},
+		[]float64{
+			0.9,
+			0.9,
+			0.4,
+			0.1,
+		},
+		[]float64{
+			0.2,
+			0.8,
+			0.5,
+			0.1,
+			0.4,
+		},
+	}
+)
+
+func TestMain(m *testing.M) {
+	// パッケージ内のテストの実行
+	code := m.Run()
+	// 終了処理
+	log2.Close()
+	// テストの終了コードで exit
+	os.Exit(code)
+}
+
+func TestWorkerCred(t *testing.T) {
+	f := 0.3
+	k := 1
+	cred := CalcWorkerCred(f, k)
+	if cred < 0 || cred > 1 {
+		t.Fatalf("want cred=%f, but %f", 0.842337, cred)
+	}
+}
+
+func TestSigma(t *testing.T) {
+	datalist := []float64{
+		0.1,
+		0.4,
+		0.2,
+		0.3,
+	}
+	sum := sigma(datalist, false, []int{})
+	if sum != 1 {
+		t.Fatalf("want sum=%f, but %f", 1.0, sum)
+	}
+	sum = sigma(datalist, false, []int{1, 3})
+	if sum >= 0.4 || sum < 0.3 {
+		t.Fatalf("want sum=%f, but %.20f", 0.3, sum)
+	}
+	sum = sigma(datalist, true, []int{})
+	if sum != 3 {
+		t.Fatalf("want sum=%f, but %f", 0.0, sum)
+	}
+
+}
+
+func TestLambda(t *testing.T) {
+	datalist := []float64{
+		0.1,
+		0.4,
+		0.2,
+		0.3,
+	}
+	lam := lambda(datalist, false, []int{})
+	if lam < 0.0024 {
+		t.Fatalf("want sum=%f, but %.20f", 1.0, lam)
+	}
+	lam = lambda(datalist, false, []int{1, 3})
+	if lam < 0.02 || lam >= 0.03 {
+		t.Fatalf("want sum=%f, but %.20f", 0.3, lam)
+	}
+	lam = lambda(datalist, true, []int{})
+	if lam < 0.3 || lam >= 0.4 {
+		t.Fatalf("want sum=%f, but %f", 0.0, lam)
+	}
+
+}
+
+func TestGroupCred(t *testing.T) {
+	groups := groups1
+	cred := CalcRGroupCred(0, groups)
+	if cred < 0.002517 || cred >= 0.002519 {
+		t.Fatalf("want cred=%f, but %f", 0.002518, cred)
+	}
+}
+
+func TestNeedWorkerCount(t *testing.T) {
+	groups := groups1
+	avgcred := 0.7
+	threshold := 0.9999999999999999
+
+	count := CalcNeedWorkerCount(avgcred, groups, threshold)
+
+	if count != 0 {
+		t.Fatalf("want count=%d, but %d", 0, count)
+	}
+
+}
---
+++ pkg/stolerance/credibility.go
@@ -0,0 +1,109 @@
+package stolerance
+
+import (
+	"yox2yox/antone/internal/log2"
+)
+
+var e = 2.718281828459045
+
+func CalcWorkerCred(f float64, reputation int) float64 {
+	return 1 - (f/(1-f))*(1/(float64(reputation)*e))
+}
+
+func CalcRGroupCred(targetidx int, groups [][]float64) float64 {
+	var goodprob = []float64{}
+	var badprob = []float64{}
+
+	for _, group := range groups {
+		good := lambda(group, false, []int{})
+		bad := lambda(group, true, []int{})
+		goodprob = append(goodprob, good)
+		badprob = append(badprob, bad)
+	}
+
+	allptn := 0.0
+	for index, prob := range goodprob {
+		allptn += prob * lambda(badprob, false, []int{index})
+	}
+	allptn += lambda(badprob, false, []int{})
+
+	cred := (goodprob[targetidx] * lambda(badprob, false, []int{targetidx})) / allptn
+
+	return cred
+}
+
+func CalcNeedWorkerCount(avgcred float64, groups [][]float64, threshold float64) int {
+
+	if threshold <= 0 || threshold >= 1 {
+		log2.Err.Printf("threshold is not propbability.(%.30f)", threshold)
+		return -1
+	}
+	if avgcred <= 0 || avgcred >= 1 {
+		log2.Err.Printf("average credibility is not propbability.(%.30f)", avgcred)
+		return -1
+	}
+
+	maxgroup := 0
+	maxcred := 0.0
+	log2.Debug.Print("start to calc groups' credibility")
+	for index, _ := range groups {
+		cred := CalcRGroupCred(index, groups)
+		if cred > maxcred {
+			maxcred = cred
+			maxgroup = index
+		}
+	}
+
+	estimate := maxcred
+	needcount := 0
+	log2.Debug.Printf("start to calc need worker count")
+	for estimate < threshold {
+		needcount += 1
+		groups[maxgroup] = append(groups[maxgroup], avgcred)
+		estimate = CalcRGroupCred(maxgroup, groups)
+	}
+
+	log2.Debug.Printf("complete to calc need worker count [%d]", needcount)
+	return needcount
+
+}
+
+func sigma(target []float64, preverse bool, except []int) float64 {
+	result := 0.0
+	for index, x := range target {
+		isExcepted := false
+		for _, i := range except {
+			if index == i {
+				isExcepted = true
+				break
+			}
+		}
+		if !isExcepted {
+			if preverse {
+				x = 1.0 - x
+			}
+			result += x
+		}
+	}
+	return result
+}
+
+func lambda(target []float64, preverse bool, except []int) float64 {
+	result := 1.0
+	for index, x := range target {
+		isExcepted := false
+		for _, i := range except {
+			if index == i {
+				isExcepted = true
+				break
+			}
+		}
+		if !isExcepted {
+			if preverse {
+				x = 1 - x
+			}
+			result *= x
+		}
+	}
+	return result
+}
